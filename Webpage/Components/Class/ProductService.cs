53f04080f4af95edd14cedffa1478a340aea7d43
AkashBakka123 <akashbakka@gmail.com>
2024-12-07 09:42:42 +0530

    Add project files.

---
+++ Webpage/Components/Class/ProductService.cs
@@ -0,0 +1,23 @@
+﻿namespace Webpage.Components.Class
+{
+    public class ProductService
+    {
+        public List<Product> Products { get; private set; } = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public int IndexValue { get; set; }
+            public int Price { get; set; }
+            public int DiscountPrice { get; set; }
+            public string ImageUrl { get; set; }
+            public int Quantity { get; set; }
+            public decimal DiscountPrecentage { get; set; }
+        }
+    }
+}
